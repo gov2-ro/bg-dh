ab5be50ccea371530ed4bd621efa14c6b3ff9dea
Alex Popescu <pax@mioritics.ro>
2024-11-29 01:21:27 +0000

    indicators list

--- components/ThemesToc.svelte
+++ components/ThemesToc.svelte
@@ -13,19 +13,19 @@
     <ChevronCircleUp />
   </div> -->
  
-
+<div class="fullToc">
 
 {#each qThemes as theme}
 <div class="level-0-wrapper mb-8 pb-8">
-    <h2 class="bg-sky-200 px-3 pt-1 pb-0 text-xl rounded-t inline-block font-bold">{theme.Name}</h2>
-    <div class="xdescription text-lg bg-sky-200 p-3  pb-4 rounded-bl rounded-br rounded-tr leading-tight text-base" style="max-width: 45em;">  {theme.Description}</div>
-    <div class="level-0   xl:columns-2 2xl:columns-3 pt-2">
+    <h2 class="bg-slate-100 px-3 pt-2 pb-0 text-xl rounded-t inline-block font-bold">{theme.Name}</h2>
+    <div class="xdescription text-lg bg-slate-100 p-3  pb-4 rounded-bl rounded-br rounded-tr leading-tight text-base"  >  {theme.Description}
+    <div class="level-0   xl:columns-2 2xl:columns-3 pt-2 mt-2">
     {#each qComponents as component}
         {#if component.ThemeID == theme.ID}
-        <div class="level-1-wrapper p-0 rounded shadow mb-4 border border-sky-300 leading-tight">
-            <h3 class="border-b border-sky-400 bg-sky-200 px-2 py-2 font-bold"> {component.Name}</h3>     
-            <div class="description bg-sky-50 p-3 ">{component.Description}</div>
-            <div class="level-1 px-3 py-2 bg-yellow-50 border-t border-sky-300 ">
+        <div class="level-1-wrapper p-0 rounded  mb-4 border border-slate-300 leading-tight group cursor-pointer">
+            <h3 class="border-b border-slate-400 bg-slate-200 px-2 py-2 font-bold"> {component.Name}</h3>     
+            <div class="description bg-slate-50 p-3 ">{component.Description}</div>
+            <div class="level-1 px-3 py-2 bg-slate-100 border shadow border-slate-300 hidden group-hover:block absolute">
             {#each qIndicators as indicator}        
                 {#if indicator.Component == component.id}
                 <div class="level-2 mt-1 mb-2">
@@ -39,14 +39,14 @@
     
         {/if}   
     {/each}
+    </div></div>
     </div>
-    </div>
-    <hr>
-    <hr>
+  
 {/each}
- 
+ </div>
 <style>
     div:empty {display: none;}  
     .level-1-wrapper {break-inside: avoid;  }
-    .description {  max-width: 60em;}
+    .level-1-wrapper:has(.description + .level-1:empty) {display: none}
+    .level-1 {max-width: 32em !important;}
 </style>
--- pages/indicator/index.md
+++ pages/indicator/index.md
@@ -1,5 +1,5 @@
 ---
-title: Indicators
+title: Full Indicators List
 ---
 
 ```sql ziToc
@@ -26,20 +26,27 @@ JOIN
     Datasets ON Indicators.Dataset = Datasets.id
 
 ```
- 
 
-<DataTable data={ziToc} rows=all groupBy=Theme_Name  groupsOpen=false >
+
+---
+
+<ThemesTocStatic />
+
+ <!--
+
+<div class="lightToc">
+<DataTable data={ziToc} rows=all groupBy=Theme_Name groupsOpen=false >
   <Column id=Theme_Name/> 
 	<Column id=Component_Name /> 
 	<Column id=Indicator_Link contentType=link linkLabel=Indicator_Name  /> 
 </DataTable>
+</div>
 
- 
-
+  
 # Detalied view
 
 Use the list below to browse Themes, Components and Indicators descriptions. <br/><small>Note: Click on <span class="bg-slate-200 px-2 py-1 text-sm rounded">Details</span> to see the indicators for each theme's components.</small>
 
 ---
 
-<ThemesTocStatic />
+-->
